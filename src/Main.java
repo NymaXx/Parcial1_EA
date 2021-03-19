51eff8fca0fc27f29e21f241f1f2814899193179
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-03-19 14:18:36 -0500

    First commit, creacion del proyecto

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			PApplet.main(Main.class.getName());
+		}
+		
+		
+	public void settings() {
+			size(1000,600);
+		}
+
+		
+	public void setup() {
+		
+		}
+		
+	public void draw() {
+		background(0);
+			
+		}
+		
+}
